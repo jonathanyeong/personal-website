6a1aef0b36137133c2637205a978669e4317ef4f
Jonathan Yeong <jonathanyeong@gmail.com>
2023-03-22 23:55:23 -0400

    Use netlify as the site url for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.jonathanyeong.com',
+  // site: 'https://www.jonathanyeong.com',
+  site: 'https://brilliant-moonbeam-1e2ea1.netlify.app',
   integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     drafts: false,
