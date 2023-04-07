582bec776c0588ea7977dbda34ce0d5d8f588fd0
Jonathan Yeong <jonathanyeong@gmail.com>
2023-04-07 12:35:48 -0400

    Fix RSS feed

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jonathan Yeong';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'I am a senior software developer at Shopify. This blog is a collection of thoughts, notes, and guides on topics that excite me.';
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,20 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
+import showDraftPosts from '../utilities/showDraftPosts';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+	const posts = (await getCollection('blog')).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+	).filter((post) => showDraftPosts(post));
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
 		items: posts.map((post) => ({
 			...post.data,
-			link: `/blog/${post.slug}/`,
+			link: `/${post.slug}/`,
 		})),
 		stylesheet: '/rss/styles.xsl',
 	});
