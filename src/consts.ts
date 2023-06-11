18edfbb102d14f81db552f2bd9e5ee887b5d0b81
Jonathan Yeong <jonathanyeong@gmail.com>
2023-06-11 18:58:50 -0400

    Update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jonathan Yeong';
-export const SITE_DESCRIPTION = 'I am a senior software developer at Shopify. This blog is a collection of thoughts, notes, and guides on topics that excite me.';
+export const SITE_DESCRIPTION = "Hey there, I'm Jonathan Yeong 👋. But you can call me Jono!\
+				I'm a Senior Developer at Shopify before that I worked at a couple of startups, and as a freelancer.\
+				This blog is a collection of thoughts, notes, and guides on topics that excite me.";
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,5 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import showDraftPosts from '../utilities/showDraftPosts';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
@@ -9,7 +8,7 @@ const parser = new MarkdownIt();
 export async function get(context) {
 	const posts = (await getCollection('blog')).sort(
 		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-	).filter((post) => showDraftPosts(post));
+	);
 
 	return rss({
 		title: SITE_TITLE,
