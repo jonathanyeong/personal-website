370cecc5ce5b23da30f6f04d143df84e1f9fb915
Jonathan Yeong <jonathanyeong@gmail.com>
2024-05-18 08:31:22 -0400

    Upgrade to all the new package versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.jonathanyeong.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  experimental: {
-    assets: true
-  },
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
   markdown: {
     drafts: false,
     shikiConfig: {
