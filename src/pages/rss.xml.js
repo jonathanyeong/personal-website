9391ff993f8ab105d17852bbd6af8f778d6f562e
Jonathan Yeong <jonathanyeong@gmail.com>
2024-05-18 10:57:49 -0400

    Filter out draft posts in rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import showDraftPosts from '../utilities/showDraftPosts';
 import sanitizeHtml from 'sanitize-html';
 import MarkdownIt from 'markdown-it';
 const parser = new MarkdownIt();
@@ -8,7 +9,7 @@ const parser = new MarkdownIt();
 export async function GET(context) {
 	const posts = (await getCollection('blog')).sort(
 		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-	);
+	).filter((post) => showDraftPosts(post));;
 
 	return rss({
 		title: SITE_TITLE,
