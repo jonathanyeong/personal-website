5020ecc5ae085315f71224cbb0831ba41196805b
Jonathan Yeong <jonathanyeong@gmail.com>
2024-05-30 18:56:18 -0400

    Update content around the site

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Jonathan Yeong';
-export const SITE_DESCRIPTION = "Hey there, I'm Jonathan Yeong 👋. But you can call me Jono!\
-				I'm a Senior Developer at Shopify before that I worked at a couple of startups, and as a freelancer.\
-				This blog is a collection of thoughts, notes, and guides on topics that excite me.";
+export const SITE_DESCRIPTION = "Hey there, I'm Jono Yeong 👋. \
+				I'm a Staff Software Engineer from Australia currently based in Canada. \
+				This blog is a collection of notes on topics that I find interesting and exciting.";
