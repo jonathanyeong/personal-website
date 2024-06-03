6dfa57e1e18b50d617d754507aaffcce81af91eb
Jonathan Yeong <jonathanyeong@gmail.com>
2024-06-03 18:09:00 -0400

    Remove console logs

--- src/utilities/filterPublishedPosts.ts
+++ src/utilities/filterPublishedPosts.ts
@@ -5,10 +5,7 @@ import hasPubDatePassed from './hasPubDatePassed';
 const filterPublishedPosts = (allPosts: CollectionEntry<"blog">[]): CollectionEntry<"blog">[] => {
   return allPosts.sort(
     (a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
-  ).filter((post) => {
-    console.log(`For Post: ${post.data.title} -- is NOT Draft Post ${!isDraftPost(post)} and hasPubDatePassed ${hasPubDatePassed(post)}`)
-    return import.meta.env.DEV || !isDraftPost(post) && hasPubDatePassed(post)
-  });
+  ).filter((post) => import.meta.env.DEV || !isDraftPost(post) && hasPubDatePassed(post));
 }
 
 export default filterPublishedPosts;
