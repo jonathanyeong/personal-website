6eb4697c7544616d1e5bbc97e9f1e8da350f51e5
Jonathan Yeong <hey@jonathanyeong.com>
2025-03-10 12:26:45 +0000

    Start redesign with the Header
    - Remove tailwind
    - Hide rest of layout
    - Start writing CSS for base layout and header!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.jonathanyeong.com',
-  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  integrations: [mdx(), sitemap(), icon()],
   markdown: {
     drafts: false,
     shikiConfig: {
