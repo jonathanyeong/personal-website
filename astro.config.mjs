e00282cfdcb7a960b86fbdeb384049e3192960a8
Jonathan Yeong <hey@jonathanyeong.com>
2025-11-19 18:58:24 -0500

    Set up sanity (following blog post)
    This now renders blog posts from sanity, but does away with my current blog
    post template.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,24 @@ import icon from "astro-icon";
 import { remarkReadingTime } from './remark-reading-time.mjs';
 import netlify from '@astrojs/netlify';
 import rehypeExternalLinks from 'rehype-external-links'
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.jonathanyeong.com',
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon(),
+    sanity({
+      projectId: 'dj97ssl8',
+      dataset: 'blog-posts',
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2025-11-19", // insert the current date to access the latest version of the API
+      studioBasePath: '/studio' // If you want to access the Studio on a route
+    }),
+    react()],
   markdown: {
     drafts: false,
     shikiConfig: {
