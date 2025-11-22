cea53c3840da0cca36315704d87fcfd293851742
Jonathan Yeong <hey@jonathanyeong.com>
2025-11-22 07:57:11 -0500

    Fix ghost admin url

--- src/lib/ghost.ts
+++ src/lib/ghost.ts
@@ -1,8 +1,7 @@
 import GhostContentAPI from '@tryghost/content-api';
 
-// Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
-    url: 'https://tes-account-mobile.ghost.io', // This is the default URL if your site is running on a local environment
+    url: 'https://jonathan-yeong.ghost.io',
     key: import.meta.env.CONTENT_API_KEY,
     version: 'v6.0',
 });
